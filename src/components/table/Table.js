a498d196c62643b9965345b42b59fac35df54f6b
Dastan Akhmetov <dastan.mvp97@gmail.com>
2020-09-17 14:33:01 +0600

    added style logic

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -19,28 +19,43 @@ export class Table extends ExcelComponent {
             const $resizer = $(event.target),
                   $parent = $resizer.closest('[data-type="resizable"]'),
                   coords = $parent.getCoords(),
-                  cells = this.$root.findAll(`[data-col="${$parent.data.col}"]`)
+                  cells = this.$root.findAll(`[data-col="${$parent.data.col}"]`),
+                  type = $resizer.data.resize,
+                  sideProp = type === 'col' ? 'bottom' : 'right'
+            let value
             $resizer.css({
-                opacity: 1
+                opacity: 1,
+                [sideProp]: '-5000px'
             })
 
             document.onmousemove = e => {
-                if ($resizer.data.resize === 'col') {
-                    const delta = Math.floor(e.pageX - coords.right),
-                          value = coords.width + delta
-                    $parent.css({width: value + 'px'})
-                    cells.forEach(el => el.style.width = value + 'px')
+                if (type === 'col') {
+                    const delta = Math.floor(e.pageX - coords.right)
+                    value = coords.width + delta
+                    $resizer.css({
+                        right: -delta + 'px'
+                    })
                 } else {
-                    const delta = Math.floor(e.pageY - coords.bottom),
-                          value = coords.height + delta
-                    $parent.css({height: value + 'px'})
+                    const delta = Math.floor(e.pageY - coords.bottom)
+                    value = coords.height + delta
+                    $resizer.css({
+                        bottom: -delta + 'px'
+                    })
                 }
             }
 
             document.onmouseup = () => {
                 document.onmousemove = null
+                document.onmouseup = null
+                const sideProp = type === 'col' ? 'width' : 'height'
+                $parent.css({[sideProp]: value + 'px'})
+                if (type === 'col') {
+                    cells.forEach(el => el.style.width = value + 'px')
+                }
                 $resizer.css({
-                    opacity: 0
+                    opacity: 0,
+                    bottom: 0,
+                    right: 0
                 })
             }
         }
