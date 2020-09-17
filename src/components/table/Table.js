dd0c751b70ce118b719e729a2c1519ccfa6283e2
Dastan Akhmetov <dastan.mvp97@gmail.com>
2020-09-17 18:02:25 +0600

    finished with resize

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,6 +1,6 @@
 import {ExcelComponent} from '@core/ExcelComponent'
 import {createTable} from '@/components/table/table.template'
-import {$} from '@core/dom';
+import {resizeHandler} from './table.resize';
 
 export class Table extends ExcelComponent {
     static className = 'excel__table'
@@ -10,54 +10,14 @@ export class Table extends ExcelComponent {
             listeners: ['mousedown']
         })
     }
+    
     toHTML() {
         return createTable(40)
     }
 
     onMousedown(event) {
         if (event.target.dataset.resize) {
-            const $resizer = $(event.target),
-                  $parent = $resizer.closest('[data-type="resizable"]'),
-                  coords = $parent.getCoords(),
-                  cells = this.$root.findAll(`[data-col="${$parent.data.col}"]`),
-                  type = $resizer.data.resize,
-                  sideProp = type === 'col' ? 'bottom' : 'right'
-            let value
-            $resizer.css({
-                opacity: 1,
-                [sideProp]: '-5000px'
-            })
-
-            document.onmousemove = e => {
-                if (type === 'col') {
-                    const delta = Math.floor(e.pageX - coords.right)
-                    value = coords.width + delta
-                    $resizer.css({
-                        right: -delta + 'px'
-                    })
-                } else {
-                    const delta = Math.floor(e.pageY - coords.bottom)
-                    value = coords.height + delta
-                    $resizer.css({
-                        bottom: -delta + 'px'
-                    })
-                }
-            }
-
-            document.onmouseup = () => {
-                document.onmousemove = null
-                document.onmouseup = null
-                const sideProp = type === 'col' ? 'width' : 'height'
-                $parent.css({[sideProp]: value + 'px'})
-                if (type === 'col') {
-                    cells.forEach(el => el.style.width = value + 'px')
-                }
-                $resizer.css({
-                    opacity: 0,
-                    bottom: 0,
-                    right: 0
-                })
-            }
+            resizeHandler(this.$root, event)
         }
     }
 }
