0745a38f2dfadfb63f0d53a826517e4a4a6a24e5
Dastan Akhmetov <dastan.mvp97@gmail.com>
2020-12-03 17:42:01 +0600

    added action to save style on cell

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -1,10 +1,12 @@
 import {storage} from '../core/utils'
+import {defaultStyles} from '@/constants'
 
 const defaultState = {
   rowState: {},
   colState: {},
   dataState: {}, // {'0:1': 'text'}
-  currentText: ''
+  currentText: '',
+  currentStyle: defaultStyles
 }
 
 export const initialState = storage('excel-state') ? storage('excel-state') : defaultState
